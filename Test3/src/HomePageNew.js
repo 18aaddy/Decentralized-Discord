e4500f6cb83ea6b7567ff8e6793073995b60a76d
practicalPanda7325 <156360185+practicalPanda7325@users.noreply.github.com>
2024-06-03 07:35:57 +0530

    Update HomePageNew.js

--- Test3/src/HomePageNew.js
+++ Test3/src/HomePageNew.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./App.css";
 import walletPic from "./img/wallet-img.jpg";
 import icon0x from "./img/0x.png";
 import iconAave from "./img/aave.jpeg";
@@ -51,12 +52,12 @@ export default function NewHomePage() {
             <div className="popup" id="popup">
                 <button onClick={() => {setPopupEnabled(false)}} data-bs-toggle="collapse" type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvasDark" aria-label="Close" id="close-button"></button>
                 <h1 id="popup-heading">COMMUNITY INFO</h1>
-                <div className="container">
-                    <img src="" id="community-pic"></img>
-                    <p id="community-name">Name</p>
-                    <p id="community-info">It is a description of the community.
-                    <br></br>Also when it was started
-                    <br></br> and member count <br></br>just checking</p>
+                <div id="container">
+                    <img src={arbitrum} id="community-pic"></img>
+                    <p id="community-name">ARBITRUM</p>
+                    <p id="community-info-1">Arbitrum (ARB) is a layer-2 scaling solution designed to address the pressing challenges of scalability and high transaction fees within the Ethereum blockchain.</p>
+                    <p id="community-info-2">Started on August 31, 2021
+                    <br></br> 1M Members </p>
                     <button onClick={handleRedirect} id="join-button">JOIN</button>
                 </div>
             </div>
@@ -88,9 +89,9 @@ export default function NewHomePage() {
        
         <div id="main-page">
           <div className="nav-bar">
-            <div className="nav-div"><button className="nav-buttons">Updates</button></div>
-            <div className="nav-div"><button className="nav-buttons">Explore</button></div>
-            <div id="search-bar-div"><input className="search-bar" type="text" placeholder="Search" /></div>
+            <button className="nav-buttons">Updates</button>
+            <button className="nav-buttons">Explore</button>
+            <input className="search-bar" type="text" placeholder="Search" />
           </div>
           
           <div id="actual-page">
@@ -113,7 +114,8 @@ export default function NewHomePage() {
                   <img className="messenger-pic" src={messenger3} alt="Messenger 3" />
                   <button className="dm-name"><p className="messenger-name">YoAvgHomie</p></button>
                 </div>
-               
+
+                <a href="http://localhost:3000"><button id="vid" className="dm-name">Video Call Feature</button></a>
 
                 <div id="profile-n-setting">
                   <img id="user-pic" src={userPic} alt="User" />
@@ -122,85 +124,79 @@ export default function NewHomePage() {
                 </div>
               </div>
             </div>
-            
-            <div id="inner-page">
-              <div id="final-inner-page">
-              
-              
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={aragon} alt="Aragon" />
-                  <p className="dao-name">ARAGON</p>
-                  <p className="member-count">Member Count</p>
-                  <button onClick={join} className="dao-join">JOIN</button>
-                </div> 
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={arbitrum} alt="Arbitrum" />
-                  <p className="dao-name">ARBITRUM</p>
-                  <p className="member-count">Member Count</p>
-                  <button onClick={join} className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={bitdao} alt="BitDAO" />
-                  <p className="dao-name">BitDAO</p>
-                  <p className="member-count">Member Count</p>
-                  <button onClick={join} className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={compound} alt="Compound" />
-                  <p className="dao-name">COMPOUND</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={dash} alt="Dash" />
-                  <p className="dao-name">DASH</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={developerDAO} alt="DeveloperDAO" />
-                  <p className="dao-name">DeveloperDAO</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={illuvium} alt="Illuvium" />
-                  <p className="dao-name">ILLUVIUM</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={makerdao} alt="MakerDAO" />
-                  <p className="dao-name">MakerDAO</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={protocolDAO} alt="ProtocolDAO" />
-                  <p className="dao-name">ProtocolDAO</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={sushiswap} alt="Sushiswap" />
-                  <p className="dao-name">SUSHISWAP</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={synthetix} alt="Synthetix" />
-                  <p className="dao-name">SYNTHETIX</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-                <div className="dao-tile">
-                  <img className="dao-search-icon" src={uniswap} alt="Uniswap" />
-                  <p className="dao-name">UNISWAP</p>
-                  <p className="member-count">Member Count</p>
-                  <button className="dao-join">JOIN</button>
-                </div>
-
-
+             
+            <div id="final-inner-page">
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={aragon} alt="Aragon" />
+                <p className="dao-name">ARAGON</p>
+                <p className="member-count">80K Members</p>
+                <button onClick={join} className="dao-join">JOIN</button>
+              </div> 
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={arbitrum} alt="Arbitrum" />
+                <p className="dao-name">ARBITRUM</p>
+                <p className="member-count">1M Members</p>
+                <button onClick={join} className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={bitdao} alt="BitDAO" />
+                <p className="dao-name">BitDAO</p>
+                <p className="member-count">85K Members</p>
+                <button onClick={join} className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={compound} alt="Compound" />
+                <p className="dao-name">COMPOUND</p>
+                <p className="member-count">99K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={dash} alt="Dash" />
+                <p className="dao-name">DASH</p>
+                <p className="member-count">89K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={developerDAO} alt="DeveloperDAO" />
+                <p className="dao-name">DeveloperDAO</p>
+                <p className="member-count">82K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={illuvium} alt="Illuvium" />
+                <p className="dao-name">ILLUVIUM</p>
+                <p className="member-count">91K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={makerdao} alt="MakerDAO" />
+                <p className="dao-name">MakerDAO</p>
+                <p className="member-count">93K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={protocolDAO} alt="ProtocolDAO" />
+                <p className="dao-name">ProtocolDAO</p>
+                <p className="member-count">95K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={sushiswap} alt="Sushiswap" />
+                <p className="dao-name">SUSHISWAP</p>
+                <p className="member-count">78K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={synthetix} alt="Synthetix" />
+                <p className="dao-name">SYNTHETIX</p>
+                <p className="member-count">84K Members</p>
+                <button className="dao-join">JOIN</button>
+              </div>
+              <div className="dao-tile">
+                <img className="dao-search-icon" src={uniswap} alt="Uniswap" />
+                <p className="dao-name">UNISWAP</p>
+                <p className="member-count">1.1M Members</p>
+                <button className="dao-join">JOIN</button>
               </div>
             </div>
           </div>
